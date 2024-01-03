1e72c4dc6774c100e13b6892b37bc5ea6454ec95
Calum Smith <hello@calum.io>
2024-01-03 18:43:54 -0500

    Support HTML entities in JSX text/attributes…
    JSX text and attributes support HTML character references (a.k.a.
    entities), and don't support ECMAScript string escape sequences.
    
    Although the [spec] calls it "historical" and threatens to change it,
    it _is_ in the spec, and the spec is pretty stable at this point.
    
    In changing this, I landed back on an idea that @maxbrunsfeld suggested
    in a [PR review] some time ago: having separate `string` and
    `jsx_string` nodes, and aliasing `jsx_string` to `string` for consumers'
    convenience. At that time, having two different node types was deemed
    unnecessary, but this adds a second, more substantive difference between
    the two, so I've brought the idea back, and stopped allowing invalid
    newlines in JS string literals, which is invalid in both JS and TS.
    
    [spec]: https://facebook.github.io/jsx/#sec-jsx-string-characters
    [PR review]: https://github.com/tree-sitter/tree-sitter-javascript/pull/140#discussion_r503399344

--- grammar.js
+++ grammar.js
@@ -601,9 +601,14 @@ module.exports = grammar({
 
     // Should not contain new lines and should not start or end with a space
     jsx_text: _ => choice(
-      /[^{}<>\n ]([^{}<>\n]*[^{}<>\n ])?/,
+      /[^{}<>\n& ]([^{}<>\n&]*[^{}<>\n& ])?/,
       /\/\/[^\n]*/,
     ),
+
+    // An entity can be named, numeric (decimal), or numeric (hexadecimal). The
+    // longest entity name is 29 characters long, and the HTML spec says that
+    // no more will ever be added.
+    jsx_html_character_reference: _ => /&(#([xX][0-9a-fA-F]{1,6}|[0-9]{1,5})|[A-Za-z]{1,30});/,
 
     jsx_expression: $ => seq(
       '{',
@@ -617,6 +622,7 @@ module.exports = grammar({
 
     _jsx_child: $ => choice(
       $.jsx_text,
+      $.jsx_html_character_reference,
       $._jsx_element,
       $.jsx_expression,
     ),
@@ -676,8 +682,36 @@ module.exports = grammar({
       )),
     ),
 
+    jsx_attribute_string: $ => choice(
+      seq(
+        '"',
+        repeat(choice(
+          alias($.unescaped_double_jsx_attribute_string_fragment, $.string_fragment),
+          $.jsx_html_character_reference,
+        )),
+        '"',
+      ),
+      seq(
+        '\'',
+        repeat(choice(
+          alias($.unescaped_single_jsx_attribute_string_fragment, $.string_fragment),
+          $.jsx_html_character_reference,
+        )),
+        '\'',
+      ),
+    ),
+
+    // Workaround to https://github.com/tree-sitter/tree-sitter/issues/1156
+    // We give names to the token() constructs containing a regexp
+    // so as to obtain a node in the CST.
+    //
+    unescaped_double_jsx_attribute_string_fragment: _ => token.immediate(prec(1, /[^"&]+/)),
+
+    // same here
+    unescaped_single_jsx_attribute_string_fragment: _ => token.immediate(prec(1, /[^'&]+/)),
+
     _jsx_attribute_value: $ => choice(
-      $.string,
+      alias($.jsx_attribute_string, $.string),
       $.jsx_expression,
       $._jsx_element,
     ),
@@ -907,12 +941,6 @@ module.exports = grammar({
     // Primitives
     //
 
-    // Here we tolerate unescaped newlines in double-quoted and
-    // single-quoted string literals.
-    // This is legal in typescript as jsx/tsx attribute values (as of
-    // 2020), and perhaps will be valid in javascript as well in the
-    // future.
-    //
     string: $ => choice(
       seq(
         '"',
@@ -936,10 +964,10 @@ module.exports = grammar({
     // We give names to the token() constructs containing a regexp
     // so as to obtain a node in the CST.
     //
-    unescaped_double_string_fragment: _ => token.immediate(prec(1, /[^"\\]+/)),
+    unescaped_double_string_fragment: _ => token.immediate(prec(1, /[^"\\\n]+/)),
 
     // same here
-    unescaped_single_string_fragment: _ => token.immediate(prec(1, /[^'\\]+/)),
+    unescaped_single_string_fragment: _ => token.immediate(prec(1, /[^'\\\n]+/)),
 
     escape_sequence: _ => token.immediate(seq(
       '\\',
