adc9d0df3e471e37ffa6bf2be89725c3471e403a
Amaan Qureshi <amaanq12@gmail.com>
2024-04-04 17:12:20 -0400

    build: update bindings and versions

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,9 +1,3 @@
-// -*- coding: utf-8 -*-
-// ------------------------------------------------------------------------------------------------
-// Copyright © 2020, tree-sitter-javascript authors.
-// See the LICENSE file in this repo for license details.
-// ------------------------------------------------------------------------------------------------
-
 //! This crate provides a JavaScript grammar for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this grammar to a
@@ -13,16 +7,14 @@
 //! use tree_sitter::Parser;
 //!
 //! let code = r#"
-//!     function double(x) {
-//!         return x * 2;
-//!     }
+//! function double(x) {
+//!     return x * 2;
+//! }
 //! "#;
 //! let mut parser = Parser::new();
-//! parser.set_language(tree_sitter_javascript::language()).expect("Error loading JavaScript grammar");
-//! let parsed = parser.parse(code, None);
-//! # let parsed = parsed.unwrap();
-//! # let root = parsed.root_node();
-//! # assert!(!root.has_error());
+//! parser.set_language(&tree_sitter_javascript::language()).expect("Error loading JavaScript grammar");
+//! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
@@ -43,8 +35,10 @@ pub fn language() -> Language {
     unsafe { tree_sitter_javascript() }
 }
 
-/// The source of the JavaScript tree-sitter grammar description.
-pub const GRAMMAR: &str = include_str!("../../grammar.js");
+/// The content of the [`node-types.json`][] file for this grammar.
+///
+/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 /// The syntax highlighting query for this language.
 pub const HIGHLIGHT_QUERY: &str = include_str!("../../queries/highlights.scm");
@@ -58,13 +52,8 @@ pub const JSX_HIGHLIGHT_QUERY: &str = include_str!("../../queries/highlights-jsx
 /// The local-variable syntax highlighting query for this language.
 pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
 
-/// The content of the [`node-types.json`][] file for this grammar.
-///
-/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
-
 /// The symbol tagging query for this language.
-pub const TAGGING_QUERY: &str = include_str!("../../queries/tags.scm");
+pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -72,7 +61,7 @@ mod tests {
     fn can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(super::language())
+            .set_language(&super::language())
             .expect("Error loading JavaScript grammar");
     }
 }
