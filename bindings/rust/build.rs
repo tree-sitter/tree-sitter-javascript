d4140333b587c3d4891d2a34e6992d9c67c5ab6b
Amaan Qureshi <amaanq12@gmail.com>
2024-04-18 13:35:15 -0400

    chore: generate

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -5,6 +5,9 @@ fn main() {
     c_config.flag("-Wno-unused-parameter");
     c_config.std("c11").include(src_dir);
 
+    #[cfg(target_env = "msvc")]
+    c_config.flag("-utf-8");
+
     let parser_path = src_dir.join("parser.c");
     c_config.file(&parser_path);
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
