04ad7cc41b8c0895008969c0f478dc837a845e4f
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-05-07 14:54:53 -0700

    fix: conditionally enable no-unused-parameter
    fix #316

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,8 +2,10 @@ fn main() {
     let src_dir = std::path::Path::new("src");
 
     let mut c_config = cc::Build::new();
-    c_config.flag("-Wno-unused-parameter");
-    c_config.std("c11").include(src_dir);
+    c_config
+        .std("c11")
+        .include(src_dir)
+        .flag_if_supported("-Wno-unused-parameter");
 
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
