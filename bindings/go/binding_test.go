ccd14aea8c8ef11f5cdb45ee245350fcc8d08ac2
Amaan Qureshi <amaanq12@gmail.com>
2024-10-14 20:44:08 -0400

    build: update bindings

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -10,6 +10,6 @@ import (
 func TestCanLoadGrammar(t *testing.T) {
 	language := tree_sitter.NewLanguage(tree_sitter_javascript.Language())
 	if language == nil {
-		t.Errorf("Error loading Javascript grammar")
+		t.Errorf("Error loading JavaScript grammar")
 	}
 }
--- bindings/python/tree_sitter_javascript/__init__.pyi
+++ bindings/python/tree_sitter_javascript/__init__.pyi
@@ -1,1 +1,8 @@
-def language() -> int: ...
+from typing import Final
+
+HIGHLIGHTS_QUERY: Final[str]
+INJECTIONS_QUERY: Final[str]
+LOCALS_QUERY: Final[str]
+TAGS_QUERY: Final[str]
+
+def language() -> object: ...
