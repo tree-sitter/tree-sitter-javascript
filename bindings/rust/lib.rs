a5beaee015fd591077642dfdaa42fab125355494
Amaan Qureshi <contact@amaanq.com>
2025-09-01 02:06:04 -0400

    build: update bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,17 +1,15 @@
-//! This crate provides JavaScript language support for the [tree-sitter][] parsing library.
+//! This crate provides JavaScript language support for the [tree-sitter] parsing library.
 //!
-//! Typically, you will use the [LANGUAGE][] constant to add this language to a
-//! tree-sitter [Parser][], and then use the parser to parse some code:
+//! Typically, you will use the [`LANGUAGE`] constant to add this language to a
+//! tree-sitter [`Parser`], and then use the parser to parse some code:
 //!
 //! ```
-//! use tree_sitter::Parser;
-//!
 //! let code = r#"
 //! function double(x) {
 //!     return x * 2;
 //! }
 //! "#;
-//! let mut parser = Parser::new();
+//! let mut parser = tree_sitter::Parser::new();
 //! let language = tree_sitter_javascript::LANGUAGE;
 //! parser
 //!     .set_language(&language.into())
@@ -20,7 +18,7 @@
 //! assert!(!tree.root_node().has_error());
 //! ```
 //!
-//! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
+//! [`Parser`]: https://docs.rs/tree-sitter/0.25.8/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
 use tree_sitter_language::LanguageFn;
@@ -29,14 +27,12 @@ extern "C" {
     fn tree_sitter_javascript() -> *const ();
 }
 
-/// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
-///
-/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+/// The tree-sitter [`LanguageFn`] for this grammar.
 pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_javascript) };
 
-/// The content of the [`node-types.json`][] file for this grammar.
+/// The content of the [`node-types.json`] file for this grammar.
 ///
-/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
+/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers/6-static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 /// The syntax highlighting query for this language.
